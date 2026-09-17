b4c1b9de1b149bc023e0de72820b76304486f95b
Kol_dev <108139427+kellishbond@users.noreply.github.com>
2026-09-17 16:32:07 +0100

    Complete: Words Length Lookup

--- challenges/main.py
+++ challenges/main.py
@@ -1,7 +1,12 @@
-def same_point(point1, point2):
-    # TODO: return True if point1 and point2 represent the same location
-    return point1 == point2
+def word_lengths(words):
+    lengths = {}
+    # TODO: loop through `words` and populate `lengths` with word -> length of word
+    for word in words:
+        if word not in lengths:
+            lengths[word] = len(word)
 
-print(same_point((1, 2), (1, 2)))
-print(same_point((1, 2), (2, 1)))
-print(same_point((0, 0), (0, 0)))
+    return lengths
+
+print(word_lengths(["cat", "elephant", "ox"]))
+print(word_lengths([]))
+print(word_lengths(["hi", "hi"]))
